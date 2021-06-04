2495fe32503fac573c949fb90ce1471eb8c9483e
Babushkina O <boriskinaos@mail.ru>
2021-06-04 23:37:13 +0300

    feat: создать файл auth.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useHistory, Redirect } from 'react-router-dom';
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import Main from './components/Main/Main';
@@ -10,6 +11,7 @@ import Profile from './components/Profile/Profile';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Navigation from './components/Navigation/Navigation';
+import auth from './utils/Auth';
 
 const initialMovies = [ 
   { 
@@ -78,7 +80,81 @@ const initialMovies = [
 
 function App(props) {
   const [isNavOpen, setIsNavOpen] = React.useState(false);
-  const [isMovieSaved, setIsMovieSaved] = React.useState(false);
+  const [loggedIn, setLoggedIn] = React.useState(false);
+  const [userName, setUserName] = React.useState('');
+  const [currentUser, setCurrentUser] = React.useState(' ');
+  const history = useHistory();
+
+  const tokenCheck = React.useCallback(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      auth.getContent(token)
+        .then((res) => {
+          if (res) {
+            setLoggedIn(true);
+            setUserName(res.name);
+            history.push("/");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    }
+  }, [history]);
+
+  React.useEffect(() => {
+    tokenCheck();
+  }, [tokenCheck])
+
+  function onLogin(mail, password) {
+    auth.authorize(mail, password)
+      .then((res) => {
+        if (res.token) {
+          localStorage.setItem('token', res.token);
+          tokenCheck();
+          history.push("/")
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        /*infoToolFail();*/
+      })
+  }
+  
+  function onRegister(email, password) {
+    auth.register(email, password)
+      .then(() => {
+        history.push('/movies');
+        /*infoToolSuccess();*/
+      })
+      /*.then((data) = {
+        auth.authorize(data.email, data.password);
+      }*/
+      .catch((err) => {
+        console.log(err);
+        /*infoToolFail();*/
+      })
+  }
+
+  React.useEffect(() => {
+    MainApi.getUser()
+      .then((res) => {
+        setCurrentUser(res);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, []);
+
+  function handleUpdateUser({ name, about }) {
+    MainApi.setUser({ name, about })
+      .then((res) => {
+        setCurrentUser(res);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
 
   function handleMenuButtonClick() {
     setIsNavOpen(true);
@@ -88,46 +164,49 @@ function App(props) {
     setIsNavOpen(false);
   }
 
-  function handleLikeButtonClick() {
-    setIsMovieSaved(true);
-  }
+
 
   return (
-    <div className="page">
-      <Switch >
-        <Route exact path="/">
-          <Header isMain={true}/>
-          <Main />
-          <Footer />
-        </Route>
-        <Route exact path="/movies">
-          <Navigation isNavOpen={isNavOpen} onNavClose={handleCloseButtonClick} isMain={false}/>
-          <Header onNavOpen={handleMenuButtonClick} />
-          <Movies onMovieLike={handleLikeButtonClick} isMovieSaved={isMovieSaved} movies={initialMovies}/>
-          <Footer />
-        </Route>
-        <Route exact path="/saved-movies">
-          <Navigation isNavOpen={isNavOpen} onNavClose={handleCloseButtonClick} isMain={false}/>
-          <Header onNavOpen={handleMenuButtonClick}/>
-          <SavedMovies movies={initialMovies} isSavedMovies={true}/>
-          <Footer />
-        </Route>
-        <Route exact path="/profile">
-          <Navigation isNavOpen={isNavOpen} onNavClose={handleCloseButtonClick} isMain={false}/>
-          <Header onNavOpen={handleMenuButtonClick}/>
-          <Profile />
-        </Route>
-        <Route exact path="/sign-up">
-          <Register />
-        </Route>
-        <Route exact path="/sign-in">
-          <Login />
-        </Route>
-        <Route exact path="/*">
-          <PageNotFound />
-        </Route> 
-      </ Switch>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="page">
+        <Switch >
+          <Route exact path="/">
+            <Header isMain={true}/>
+            <Main />
+            <Footer />
+          </Route>
+          <Route exact path="/movies">
+            <Navigation isNavOpen={isNavOpen} onNavClose={handleCloseButtonClick} isMain={false}/>
+            <Header onNavOpen={handleMenuButtonClick} />
+            <Movies movies={initialMovies}/>
+            <Footer />
+          </Route>
+          <Route exact path="/saved-movies">
+            <Navigation isNavOpen={isNavOpen} onNavClose={handleCloseButtonClick} isMain={false}/>
+            <Header onNavOpen={handleMenuButtonClick}/>
+            <SavedMovies movies={initialMovies} isSavedMovies={true}/>
+            <Footer />
+          </Route>
+          <Route exact path="/profile">
+            <Navigation isNavOpen={isNavOpen} onNavClose={handleCloseButtonClick} isMain={false}/>
+            <Header onNavOpen={handleMenuButtonClick}/>
+            <Profile userName={userName} onUpdateUser={handleUpdateUser}/>
+          </Route>
+          <Route exact path="/sign-up">
+            <Register handleRegister={onRegister}/>
+          </Route>
+          <Route exact path="/sign-in">
+            <Login handleLogin={onLogin}/>
+          </Route>
+          <Route exact path="/*">
+            <PageNotFound />
+          </Route>
+          <Route>
+            {loggedIn ? <Redirect to="/movies" /> : <Redirect to="/sign-in" />}
+          </Route>
+        </ Switch>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,20 +1,47 @@
 import React from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
 
 function Profile(props) {
+
+  const currentUser = React.useContext(CurrentUserContext);
+  const [name, setName] = React.useState(' ');
+  const [password, setPassword] = React.useState(' ');
+  
+  React.useEffect(() => {
+    setName(String(currentUser.name));
+    setPassword(String(currentUser.password));
+  }, [currentUser]);
+
+  function handleChangeName(e) {
+    setName(e.target.value);
+  }
+
+  function handleChangePassword(e) {
+    setPassword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    props.onUpdateUser({
+      name,
+      password,
+    });
+  }
   
     return (
       <div className="profile">
         <div className="profile__container">
-        <form className="profile__form" name="form-element">
+        <form onSubmit={handleSubmit} className="profile__form" name="form-element">
         <div className="profile__form-container">
-          <h2 className="profile__title">Привет, Ольга!</h2>
+          <h2 className="profile__title">`Привет, ${props.name}!`</h2>
           <div className="profile__input-container">
             <p className="profile__input-name">Имя</p>
-            <input type="text" value="Ольга" minLength="2" maxLength="40" required className="profile__input" />
+            <input type="text" onClick={handleChangeName} value={name} minLength="2" maxLength="40" required className="profile__input" />
           </div>
           <div className="profile__input-container profile__input-container_last">
             <p className="profile__input-name">Email</p>
-            <input type="email" required name="input-email" value="babushkina.os@yandex.ru" className="profile__input" />
+            <input type="email" required name="input-email" onClick={handleChangePassword} value={password} className="profile__input" />
           </div>
         </div>
         <div className="profile__form-container">
